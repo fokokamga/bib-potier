62c87fd35339e6a3b379c7bff6f3e38bfc08e196
Florin <fokokamga@yahoo.fr>
2019-06-02 19:54:55 +0200

    login page and route

--- potier-library/src/app/app-routing.module.ts
+++ potier-library/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { ShopCardComponent } from './shop-card/shop-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'books/', component: HomepageComponent },
+  { path: 'books/id', component: BookDetailComponent },
+  { path: 'card-recap', component: ShopCardComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
